38cf652a76bd5106062c82cd4b33b75ce128cbe1
johnl9152 <92328984+johnl9152@users.noreply.github.com>
2021-12-09 11:37:50 -0800

    adjusted

--- Queries/Employees_Database_challenge.sql
+++ Queries/Employees_Database_challenge.sql
@@ -27,3 +27,19 @@ GROUP BY ut.title
 ORDER BY COUNT DESC;
 
 SELECT * FROM retiring_titles;
+
+-- Deliverable 2
+
+-- Create "mentorship_eligibility" table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no
+
+SELECT * FROM mentorship_eligibility;
+
